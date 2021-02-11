0fcf4b86e730b4ffd413328dd8e891e7e1d8117a
Andrew <andrew.ku93@gmail.com>
2021-02-11 23:20:00 +0000

    add additional error handling for fetchMyIP

--- iss.js
+++ iss.js
@@ -3,9 +3,18 @@ const request = require("request");
 
 const fetchMyIP = (callback) => {
   request("https://api.ipify.org/?format=json", (error, response, body) => {
+    // IF BAD REQUEST
     if (error) {
       callback(error);
+      return;
     }
+    
+    // IF RESPONSE IS NOT GOOD - 200
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+    }
+
     const ip = JSON.parse(body);
     callback(error, ip.ip);
   });
@@ -13,4 +22,4 @@ const fetchMyIP = (callback) => {
 
 module.exports = {
   fetchMyIP,
-}
+};
